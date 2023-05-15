e2478404ac889547790eaa04e17a07faf10e28f6
Janewary <cinderellakkong@gmail.com>
2023-05-16 01:42:45 +0900

    답변, 카테고리 관리자

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer, Category
 
 class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['subject']
 
+class AnswerAdmin(admin.ModelAdmin):
+    search_fields = ['content']
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['category.name']
+
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Category, CategoryAdmin)
+
